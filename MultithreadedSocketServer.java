0cf6de5d20b88210cce000da75abd56923142253
Mahaning Hubballi <92427624+Mahaning@users.noreply.github.com>
2022-04-28 16:03:18 +0530

    Update MultithreadedSocketServer.java

--- MultithreadedSocketServer.java
+++ MultithreadedSocketServer.java
@@ -1,24 +1,43 @@
-import java.net.*; 
+import java.net.*;
 import java.io.*;
+
+public class MultithreadedSocketServer {
+  public static void main(String[] args) throws Exception {
+    try{
+      ServerSocket server=new ServerSocket(8888);
+      int counter=0;
+      System.out.println("Server Started ....");
+      while(true){
+        counter++;
+        Socket serverClient=server.accept();  
+        System.out.println(" >> " + "Client No:" + counter + " started!");
+        ServerClientThread sct = new ServerClientThread(serverClient,counter); 
+        sct.start();
+      }
+    }catch(Exception e){
+      System.out.println(e);
+    }
+  }
+}
 
 class ServerClientThread extends Thread {
   Socket serverClient;
   int clientNo;
-  String squre;
+  int squre;
   ServerClientThread(Socket inSocket,int counter){
     serverClient = inSocket;
     clientNo=counter;
   }
- 
   public void run(){
     try{
       DataInputStream inStream = new DataInputStream(serverClient.getInputStream());
       DataOutputStream outStream = new DataOutputStream(serverClient.getOutputStream());
       String clientMessage="", serverMessage="";
-      while(!clientMessage.equals("exit")){
+      while(!clientMessage.equals("bye")){
         clientMessage=inStream.readUTF();
-        System.out.println("From Client-" +clientNo+ ": message is :"+clientMessage);
-        serverMessage="From Client to Server-" + clientNo + " mesage " + "sent succsfully";
+        System.out.println("From Client-" +clientNo+ ": Number is :"+clientMessage);
+        
+        serverMessage="From Client-" + clientNo + "Message \n" + clientMessage;
         outStream.writeUTF(serverMessage);
         outStream.flush();
       }
@@ -28,26 +47,7 @@ class ServerClientThread extends Thread {
     }catch(Exception ex){
       System.out.println(ex);
     }finally{
-      System.out.println("Client -" + clientNo + " exited!! ");
-    }
-  }
-}
-//Thread class for MultithreadedSocketServer
-public class MultithreadedSocketServer {
-  public static void main(String[] args) throws Exception {
-    try{
-      ServerSocket server=new ServerSocket(8888);
-      int counter=0;
-      System.out.println("Server Started ....");
-      while(true){
-        counter++;
-        Socket serverClient=server.accept();  //server accept the client connection request
-        System.out.println(" >> " + "Client No:" + counter + " connected!");
-        ServerClientThread sct = new ServerClientThread(serverClient,counter); //send  the request to a separate thread
-        sct.start();
-      }
-    }catch(Exception e){
-      System.out.println(e);
+      System.out.println("Client -" + clientNo + " exit!! ");
     }
   }
 }
